5a89ab8bbfe29f69d5565016b32aa427529a3355
Sandra Abernathy <sandraabernathy@Sandras-MacBook-Pro.local>
2019-06-27 13:59:44 -0600

    hapter 3 complete - Made the ring and gave it to Frodo

--- js/app.js
+++ js/app.js
@@ -100,11 +100,12 @@ function keepItSecretKeepItSafe() {
   console.log("3: keepItSecretKeepItSafe");
 
   // create a div with an id of `'the-ring'`
-
+const div = document.createElement('div'); 
+div.setAttribute= ('id','the-ring');
   // give the div a class of `'magic-imbued-jewelry'`
-
+div.className='magic-imbued-jewelry';
   // add the ring as a child of `Frodo`
-
+document.querySelector('.hobbit').appendChild(div)
 }
 
 // COMMIT YOUR WORK
